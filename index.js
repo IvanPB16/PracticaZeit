8b920e71b2c9b59f49d102e9888de18b692611d0
IvanPB16 <ivan.peralta.b@outlook.com>
2020-04-16 14:22:24 -0500

    cambios

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 
 app.use(express.static(__dirname + '/public/'));
 
-app.listen('3000', function() {
-  console.log('Hola mundo');
+app.set('port',process.env.PORT || 3000);
+
+app.listen(3015,()=>{
+	console.log("server run");
 });
